cb99f178eb992b7d3420947cbeb2ed2d54f3f1ee
chaitany <chaitanyjanmale@gmail.com>
2022-07-23 17:11:15 +0530

    added styles.css but remained with the logic part of project

---
+++ index.js
@@ -0,0 +1,28 @@
+
+/*let Person1 = []
+let Person2 = []
+let matchedFields = []
+function oper(){
+for (let i = 0; i < Person1.length; i++) {
+    Person1[i] == Person2[i] ? matchedFields.push(i) : null
+}
+
+if (matchedFields.length == Person1.length) {
+    alert("100%"+" "+"Matched")
+} else {
+    let matchedPercentage = Math.floor((matchedFields.length / Person1.length) * 100)
+    alert(matchedPercentage + "%"+"Matched")
+}
+}
+/* ===>this doesnt work found a bug function compare(val1,val2,index){
+    val1==val2?matchedFields.push(index):null
+} */
+/* Person1.map((val1,index)=>Person2.map((val2,index)=>compare(val1,val2,index))) 
+function submi(){
+    var c=document.getElementById("input1").value
+    var d=document.getElementById("input2").value
+    Person1.push(c)
+    Person2.push(d)
+    oper()
+}
+*/
