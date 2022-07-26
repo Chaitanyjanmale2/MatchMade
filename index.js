7c167b485c4f4df812345325ef637f08921472b4
chaitany <chaitanyjanmale@gmail.com>
2022-07-26 17:48:05 +0530

    added graph to show comparesion and also added

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-
+let Result
 let Person1 = []
 let Person2 = []
 let matchedFields = []
 function oper(){
-    console.log(Person1,Person2)
+    //console.log(Person1,Person2)
 for (let i = 0; i < Person1.length; i++) {
     Person1[i] == Person2[i] ? matchedFields.push(i) : null
+
 }
-console.log(matchedFields)
+
+
 if (matchedFields.length == Person1.length) {
-    alert("100%"+" "+"Matched")
-    
+    Result =100    
 
 } else {
     let matchedPercentage = Math.floor((matchedFields.length / Person1.length) * 100)
-    alert(matchedPercentage + "%"+"Matched")
+    Result=matchedPercentage
     
 }
 }
@@ -24,6 +25,7 @@ if (matchedFields.length == Person1.length) {
 } */
 /* Person1.map((val1,index)=>Person2.map((val2,index)=>compare(val1,val2,index))) */
 function submi(event){
+    Result
     Person1 = []
     Person2 = []
     matchedFields=[]
@@ -34,18 +36,13 @@ function submi(event){
     var b=document.getElementById("Color-selectp2").value
     
     if((c||d||a||b)==="Open this select menu"){
-        Toastify({
-            text: "Please Fill All Input Fields",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "bottom", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            
-            onClick: function(){} // Callback after click
-          }).showToast();
+         
+        
+          document.getElementById("modalb").innerHTML=`<div class="alert alert-danger" role="alert">
+          please Fill Fields To see Results!
+        </div>`
+
+
     }
     if((c||d||a||b)!=="Open this select menu"){
         Person1.push(c)
@@ -53,8 +50,41 @@ function submi(event){
         Person2.push(a)
         Person2.push(b)
         oper()
+        console.log(Result)
+        function GetResult(){
+            if(Result==100){
+                return [100,0]
+            }
+            if(Result==50){
+                return [50,50]
+            }
+        }
+        const ctx = document.getElementById('myChart');
+const myChart = new Chart(ctx, {
+  type: 'doughnut',
+    data:  {
+        labels: [
+          'Matched ',
+          'Unmatched',
+          
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: GetResult(),
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+          ],
+          hoverOffset: 4
+        }]
+      }
+});
     }
-  
+  document.getElementById('ResultDD').innerHTML="You got "+Result+"%"+"Match"
      
 }
+
+
+
+
 
